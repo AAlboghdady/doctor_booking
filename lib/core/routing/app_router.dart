575fc14abfc802971815e44ee77a96238bc212ff
Madar iOS <abdurrahman.alboghdady@gmail.com>
2024-03-03 12:50:01 +0300

    finish login end point functionality and validation
    - added di
    - added AppRegex
    - integrated with login end point
    - added login repo & cubit
    - added login states with validation

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,7 +1,11 @@
+import 'package:doctor_booking/core/di/dependency_injection.dart';
 import 'package:doctor_booking/core/routing/routes.dart';
+import 'package:doctor_booking/features/auth/login/logic/cubit/login_cubit.dart';
 import 'package:doctor_booking/features/auth/login/ui/login_screen.dart';
+import 'package:doctor_booking/features/home/ui/home_screen.dart';
 import 'package:doctor_booking/features/onboarding/onboarding_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppRouter {
   Route generateRoute(RouteSettings settings) {
@@ -9,7 +13,13 @@ class AppRouter {
       case Routes.onBoardingScreen:
         return MaterialPageRoute(builder: (_) => const OnBoardingScreen());
       case Routes.loginScreen:
-        return MaterialPageRoute(builder: (_) => const LoginScreen());
+        return MaterialPageRoute(
+            builder: (_) => BlocProvider(
+                  create: (context) => getIt<LoginCubit>(),
+                  child: const LoginScreen(),
+                ));
+      case Routes.homeScreen:
+        return MaterialPageRoute(builder: (_) => const HomeScreen());
       default:
         return MaterialPageRoute(
           builder: (_) => Scaffold(
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,10 @@ import 'package:doctor_booking/core/routing/app_router.dart';
 import 'package:doctor_booking/doc_app.dart';
 import 'package:flutter/material.dart';
 
+import 'core/di/dependency_injection.dart';
+
 void main() {
+  setupGetIt();
   runApp(DocApp(
     appRouter: AppRouter(),
   ));
